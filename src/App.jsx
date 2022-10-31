99da77172b2fab3bf54f3fb9866dfeb938f6a44d
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-31 10:10:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Weather from './Components/Weather'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Weather />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Weather.css"
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+       hello
+    </div>
+  )
+}
+
+export default Weather
